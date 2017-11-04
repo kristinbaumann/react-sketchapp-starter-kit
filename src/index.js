467994aee81aa7054f0910165f7716a2d7f06f92
Kristin Baumann <mail@kristin-baumann.com>
2017-11-04 17:46:21 +0100

    Updated to react 16 and skpm builder

--- src/index.js
+++ src/index.js
@@ -4,23 +4,14 @@ import { render, Artboard, Text, View, Image } from 'react-sketchapp';
 const App = () => (
     <Artboard 
         name="App" 
-        style={{ 
-            top: 0,
-            left: 0,
-            height: 142,
-            width: 583,
-            backgroundColor: '#333'
-        }}>
-        <View name="App-View" style={{ backgroundColor: '#f1f1f1', width: '100%', height: '100%', margin: 5}}>
-            <Image 
+        style={{ top: 0, left: 0, height: 142, width: 583 }}>
+        <View name="App-View" style={{ backgroundColor: '#f1f1f1', width: '100%', height: '100%', borderColor: '#333', borderWidth: 5 }}>
+            <Image name="App-Image"
                 source="https://raw.githubusercontent.com/kristinbaumann/react-sketchapp-starter-kit/master/src/img/logo.png"
-                name="App-Image"
                 style={{ width: '100%', height: 132 }}
-                ></Image>
+            ></Image>
         </View>
     </Artboard>
 )
 
-export default (context) => {
-    render(<App /> , context.document.currentPage())
-}   
+export default (context) => render(<App /> , context.document.currentPage())
