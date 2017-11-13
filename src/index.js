0d2fe5d84fd3392d478bd150fe513ce77acb2b5f
Kristin Baumann <mail@kristin-baumann.com>
2017-11-13 22:00:54 +0100

    Remove unused code

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import { render, Artboard, Text, View, Image } from 'react-sketchapp';
+import { render, Artboard, View, Image } from 'react-sketchapp';
 
 const App = () => (
-    <Artboard 
-        name="App" 
-        style={{ top: 0, left: 0, height: 142, width: 583 }}>
-        <View name="App-View" style={{ backgroundColor: '#f1f1f1', width: '100%', height: '100%', borderColor: '#333', borderWidth: 5 }}>
-            <Image name="App-Image"
-                source="https://raw.githubusercontent.com/kristinbaumann/react-sketchapp-starter-kit/master/src/img/logo.png"
-                style={{ width: '100%', height: 132 }}
-            ></Image>
+    <Artboard style={{height: 142, width: 583}}>
+        <View style={{ backgroundColor: '#f1f1f1', borderColor: '#333', borderWidth: 5 }}>
+            <Image source="https://goo.gl/uTKYht" style={{ height: 132 }}></Image>
         </View>
     </Artboard>
 )
