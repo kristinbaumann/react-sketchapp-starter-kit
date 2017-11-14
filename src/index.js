f3f6d01b5fcbeb87b76331174116d9453ce40a87
Kristin Baumann <mail@kristin-baumann.com>
2017-11-14 21:14:24 +0100

    Simplified example

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { render, Artboard, View, Image } from 'react-sketchapp';
 
 const App = () => (
-    <Artboard style={{height: 142, width: 583}}>
+    <Artboard>
         <View style={{ backgroundColor: '#f1f1f1', borderColor: '#333', borderWidth: 5 }}>
-            <Image source="https://goo.gl/uTKYht" style={{ height: 132 }}></Image>
+            <Image source="https://goo.gl/uTKYht" style={{ height: 132, width: 580 }} />
         </View>
     </Artboard>
 )
